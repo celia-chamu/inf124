45f8b407b7307ac3585a135e08bcb86418a540fc
Rudy Xie <rudy11923@gmail.com>
2025-04-17 12:57:58 -0700

    Added header component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,11 @@
 import Image from "next/image";
 import ListingCard from "@/components/Listings/ListingCard";
 import Navbar from "@/components/shared/Navbar";
-
+import Header from "@/components/header/header";
 export default function Home() {
   return (
     <div>
-      <Navbar />
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
-    <ListingCard
-        id="abc123"
-        imageUrl="next/image.png"
-        title="peter anteater"
-        price={129.99}
-      />
-      </div>
-      </div>
+      <Header />
+    </div>
   );
 }
---
+++ frontend/src/components/header/header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <div className="flex justify-center pt-2 w-screen">
+            <nav className="flex h-16 w-screen items-center justify-between border bg-cyan-950">
+                <div className="flex justify-start ml-5"> 
+                    <text className="text-3xl">ZotMarket</text>
+                </div>
+                <div className="flex justify-end mr-5">
+                    <button className = "border-1 p-2 pl-5 pr-5 rounded-2xl">Login</button>
+                </div>
+
+            </nav>
+        </div>
+    );
+}
