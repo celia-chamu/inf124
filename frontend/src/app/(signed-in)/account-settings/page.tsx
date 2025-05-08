bbb036886e75af23c9f0aedbed52e1bb1128f48f
Rudy Xie <rudy11923@gmail.com>
2025-05-08 15:40:32 -0700

    fixed background color for account settings

--- frontend/src/app/(signed-in)/account-settings/page.tsx
+++ frontend/src/app/(signed-in)/account-settings/page.tsx
@@ -1,15 +1,15 @@
 function AccountSettings() {
     return (
         <div className="flex flex-col items-center md:items-start w-full lg:ml-[20vw]">
-            <div className="bg-white h-50 mt-20">
+            <div className="h-50 mt-20">
                 <img className="rounded-full h-50 inline-block" src="https://dummyimage.com/150x150/000/fff"/>
                 <p className="inline-block text-xl ml-4">First Name</p>
             </div>
 
-            <h1 className="bg-white text-4xl font-bold pt-8 mb-8">Account</h1>
+            <h1 className="text-4xl font-bold pt-8 mb-8">Account</h1>
 
             <div className="flex">
-                <div className="bg-white h-40 inline-block w-2xs xs:w-sm sm:w-sm xl:w-xl lg:w-lg md:w-md">
+                <div className="h-40 inline-block w-2xs xs:w-sm sm:w-sm xl:w-xl lg:w-lg md:w-md">
                     <p className="text-lg">Name</p>
                     <div className="bg-gray-400 h-1"/>
                     <p className="pt-4 text-lg">Email</p>
@@ -18,7 +18,7 @@ function AccountSettings() {
                     <div className="bg-gray-400 h-1"/>
                 </div>
 
-                <div className="bg-white h-40 inline-block w-[2vw]">
+                <div className="h-40 inline-block w-[2vw]">
                     <button className="text-lg font-bold cursor-pointer">Edit</button>
                     <div className="bg-gray-400 h-1"/>
                     <button className="pt-4 text-lg font-bold cursor-pointer">Edit</button>
@@ -28,15 +28,15 @@ function AccountSettings() {
                 </div>
             </div>
 
-            <h1 className="bg-white text-4xl font-bold pt-8 mb-8">Notifications</h1>
+            <h1 className="text-4xl font-bold pt-8 mb-8">Notifications</h1>
 
             <div className="flex">
-                <div className="bg-white h-40 inline-block w-2xs xs:w-sm sm:w-sm xl:w-xl lg:w-lg md:w-md">
+                <div className=" h-40 inline-block w-2xs xs:w-sm sm:w-sm xl:w-xl lg:w-lg md:w-md">
                     <p className="text-lg">Notification Preference</p>
                     <div className="bg-gray-400 h-1"/>
                 </div>
 
-                <div className="bg-white h-40 inline-block w-[2vw]">
+                <div className=" h-40 inline-block w-[2vw]">
                     <button className="text-lg font-bold cursor-pointer">Edit</button>
                     <div className="bg-gray-400 h-1"/>
                 </div>
