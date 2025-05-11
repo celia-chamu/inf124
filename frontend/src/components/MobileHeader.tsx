d8825962c92b15d6cf3430486ba90cd5927d2d00
Ydureix <rudy11923@gmail.com>
2025-05-10 19:10:49 -0700

    Added ZotMarket logo to mobileheader

--- frontend/src/components/MobileHeader.tsx
+++ frontend/src/components/MobileHeader.tsx
@@ -1,14 +1,18 @@
-
 import Link from 'next/link'
-
+import Image from 'next/image'
 
 export default function MobileHeader() {
-
     return (
         <nav className="sticky top-0 z-50 flex h-16 w-full items-center justify-center bg-primary">
             <div className="ml-5 justify-center">
                 <Link href="/market">
-                    <p className="text-3xl text-yellow-500">ZotMarket</p>
+                    <Image
+                        src="/images/ZotMarket.png"
+                        alt="ZotMarket Logo"
+                        width={120}
+                        height={20}
+                        className=" object-contain"
+                    />
                 </Link>
             </div>
         </nav>
