57b212897c58fddc41ea4b1361ff40d49b4637cd
Ivan Quon <70609060+ivanquon@users.noreply.github.com>
2025-06-05 23:25:22 -0700

    Adjusted Inbox Buttons

--- frontend/src/app/(signed-in)/inbox/page.tsx
+++ frontend/src/app/(signed-in)/inbox/page.tsx
@@ -8,103 +8,116 @@ import Link from 'next/link'
 import api from '@/app/api/api'
 
 export interface Conversation {
-  conversation_id: number;
-  seller: string;
-  buyer: string;
-  started_at: Date;
-  last_message_at?: Date; 
-  last_message_preview?: string;
+    conversation_id: number
+    seller: string
+    buyer: string
+    started_at: Date
+    last_message_at?: Date
+    last_message_preview?: string
 }
 
 function Inbox() {
     const [view, setView] = useState('buyers')
     const { data: session } = useSession()
-    const [conversations, setConversations] = useState<Conversation[]>([]);
-    const [loading, setLoading] = useState(true);
-
+    const [conversations, setConversations] = useState<Conversation[]>([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchConversations = async () => {
             setLoading(true)
-            if (!session?.user?.email) return;
+            if (!session?.user?.email) return
             if (view === 'buyers') {
                 try {
-                    const response = await api.get("/fetch-conversations", {
+                    const response = await api.get('/fetch-conversations', {
                         params: {
                             user: session.user?.email,
-                            type: "buyer"
-                        }
-                    });
-                    setConversations(response.data.flat());
+                            type: 'buyer',
+                        },
+                    })
+                    setConversations(response.data.flat())
                 } catch (error) {
-                    console.error("Error fetching conversations:", error);
+                    console.error('Error fetching conversations:', error)
                     setConversations([])
                 }
-            }
-            else{
+            } else {
                 try {
-                    const response = await api.get("/fetch-conversations", {
+                    const response = await api.get('/fetch-conversations', {
                         params: {
                             user: session.user?.email,
-                            type: "seller"
-                        }
-                    });
-                    setConversations(response.data.flat());
+                            type: 'seller',
+                        },
+                    })
+                    setConversations(response.data.flat())
                 } catch (error) {
-                    console.error("Error fetching conversations:", error);
+                    console.error('Error fetching conversations:', error)
                     setConversations([])
                 }
-
             }
             setLoading(false)
-        };
+        }
 
-        fetchConversations();
-        }, [view, session?.user?.email]);
+        fetchConversations()
+    }, [view, session?.user?.email])
 
     const messages = useMemo(() => {
-        return conversations.map((convo) => view === "buyers" ? (
-            <Link key={convo.conversation_id} href={`/message/${convo.seller.split('@')[0]}`}>
-            <Message
-                key={convo.seller}
-                username={view === 'buyers' ? convo.seller : convo.buyer}
-                textMessage={convo.last_message_preview ?? ""}
-            />
-            </Link>
-        ): (
-            <Link key={convo.conversation_id} href={`/message/${convo.buyer.split('@')[0]}`}>
-            <Message
-                key={convo.seller}
-                username={view === 'buyers' ? convo.seller : convo.buyer}
-                textMessage={convo.last_message_preview ?? ""}
-            />
-            </Link>
-        ))
-    }, [conversations, view]);
+        return conversations.map((convo) =>
+            view === 'buyers' ? (
+                <Link
+                    key={convo.conversation_id}
+                    href={`/message/${convo.seller.split('@')[0]}`}
+                >
+                    <Message
+                        key={convo.seller}
+                        username={
+                            view === 'buyers' ? convo.seller : convo.buyer
+                        }
+                        textMessage={convo.last_message_preview ?? ''}
+                    />
+                </Link>
+            ) : (
+                <Link
+                    key={convo.conversation_id}
+                    href={`/message/${convo.buyer.split('@')[0]}`}
+                >
+                    <Message
+                        key={convo.seller}
+                        username={
+                            view === 'buyers' ? convo.seller : convo.buyer
+                        }
+                        textMessage={convo.last_message_preview ?? ''}
+                    />
+                </Link>
+            )
+        )
+    }, [conversations, view])
 
     return (
         <div className="flex flex-col w-full">
             <div className="bg-(--sidebar-button-background) rounded-sm mt-4 p-4 gap-8 flex items-center">
                 <Button
-                    className="rounded-2x1 cursor-pointer"
+                    className="rounded-2x1 cursor-pointer grow"
                     onClick={() => setView('buyers')}
                     variant="ListingZot"
                 >
                     Buyers
                 </Button>
                 <Button
-                    className="rounded-2x1 cursor-pointer"
+                    className="rounded-2x1 cursor-pointer grow"
                     onClick={() => setView('sellers')}
                     variant="ListingZot"
                 >
                     Sellers
                 </Button>
             </div>
-            {!loading ? 
-            (<div className="bg-(--sidebar-button-background) rounded-sm mt-4 p-8 h-180">
-                {messages}
-            </div>): <div className="bg-(--sidebar-button-background) rounded-sm mt-4 p-8 h-180"> </div>
-            }
+            {!loading ? (
+                <div className="bg-(--sidebar-button-background) rounded-sm mt-4 p-8 h-180">
+                    {messages}
+                </div>
+            ) : (
+                <div className="bg-(--sidebar-button-background) rounded-sm mt-4 p-8 h-180">
+                    {' '}
+                </div>
+            )}
         </div>
     )
 }
