f8c0653a834b1a4bf28b7878c7b153871f4dc1c9
ydureix <rudy11923@gmail.com>
2025-06-07 23:55:46 -0700

    Updated api

--- frontend/src/app/api/api.tsx
+++ frontend/src/app/api/api.tsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',  // or your backend URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000',
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 export default api;
