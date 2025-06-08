513238656205608fc620c0b3467cd4e1a60b2f0b
ydureix <rudy11923@gmail.com>
2025-06-07 21:09:05 -0700

    Update eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -17,6 +17,8 @@ const eslintConfig = [
       '@next/next/no-page-custom-font': 'off',
       "@typescript-eslint/no-unused-vars": "off",
       "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@next/next/no-img-element": "off",
     },
   },
 ];
