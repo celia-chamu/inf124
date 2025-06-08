7f66fa3ef76b403f5a64243772e66f756a20cbbc
Ivan Quon <70609060+ivanquon@users.noreply.github.com>
2025-06-08 00:43:49 -0700

    API Fixes

--- backend/api.py
+++ backend/api.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # or ["*"] for all origins (dev only)
+    allow_origins=["http://localhost:3000", "https://zot-market.vercel.app"],  # or ["*"] for all origins (dev only)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
