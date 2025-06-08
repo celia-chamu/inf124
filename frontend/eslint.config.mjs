c2847cd673febfcc9ee159de54ffd927cce1aa24
ydureix <rudy11923@gmail.com>
2025-06-07 21:06:50 -0700

    Update elsint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
       "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
     },
   },
 ];
