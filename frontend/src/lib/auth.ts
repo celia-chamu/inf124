481416a2b78a3d927769bf89c1dc7528186479b1
ydureix <rudy11923@gmail.com>
2025-06-07 21:23:10 -0700

    Try this

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,4 +1,29 @@
-import { signIn, signOut } from 'next-auth/react';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { NextAuthOptions } from 'next-auth';
 
-export const handleSignIn = () => signIn('google', {callbackUrl: '/market'});
-export const handleSignOut = () => signOut();
+export const authOptions: NextAuthOptions = {
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ profile, account }) {
+      if (account?.provider === 'google') {
+        if (!profile?.email?.endsWith('@uci.edu')) {
+          return false;
+        }
+        // your other logic here...
+        return true;
+      }
+      return '/unauthorized';
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
